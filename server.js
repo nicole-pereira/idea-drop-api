7f054d9a1e06a7c7a909fe580ad495a309941a6a
Nicole <npereira@confidencialimobiliario.com>
2026-06-12 10:51:42 +0100

    Idea Routes

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import ideaRouter from './routes/ideaRoutes.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/ideas', ideaRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
