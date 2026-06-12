c9b6f4ce656b7c403b9a17b9278cd38d7dccb6b5
Nicole <npereira@confidencialimobiliario.com>
2026-06-12 12:11:27 +0100

    Idea model + fetch ideas

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -1,29 +1,39 @@
 import express from 'express';
 const router = express.Router();
+import Idea from '../models/Idea.js';
+import mongoose from 'mongoose';
 
 // @route         GET /api/ideas
 // @description   Get all ideas
 // @access        Public
-router.get('/', (req, res) => {
-  const ideas = [
-    {
-      id: 1,
-      title: 'Idea 1',
-      description: 'This is idea 1',
-    },
-    {
-      id: 2,
-      title: 'Idea 2',
-      description: 'This is idea 2',
-    },
-    {
-      id: 3,
-      title: 'Idea 3',
-      description: 'This is idea 3',
-    },
-  ];
+router.get('/', async (req, res, next) => {
+  try {
+    const ideas = await Idea.find();
+    res.json(ideas);
+  } catch (err) {
+    next(err);
+  }
+});
 
-  res.json(ideas);
+// @route         GET /api/ideas/:id
+// @description   Get single idea
+// @access        Public
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(404);
+      throw new Error('Idea not found');
+    }
+    const idea = await Idea.findById(id);
+    if (!idea) {
+      res.status(404);
+      throw new Error('Idea not found');
+    }
+    res.json(idea);
+  } catch (err) {
+    next(err)
+  }
 });
 
 // @route         POST /api/ideas
