dcc90d3340645e700436d7bd35c1c580f14bf2e0
Nicole <npereira@confidencialimobiliario.com>
2026-06-12 17:02:20 +0100

    Use refresh token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from '../models/User.js'
+import { jwtVerify } from 'jose';
+import { JWT_SECRET } from '../utils/getJwtSecret.js';
 import{ generateToken } from '../utils/generateToken.js'
 
 
@@ -116,6 +118,45 @@ router.post('/logout', (req, res) => {
     });
     res.status(200).json({message: 'Logged out successfully'})
 });
+
+// @route         POST ap/auth/refresh
+// @description   Generate new access token from refresh token
+// @access        Public (need valid refresh token in cookie)
+router.post('/refresh', async (req, res, next) => {
+    try {
+        const token = req.cookies?.refreshToken;
+        console.log('Refreshing token...');
+
+        if (!token)  {
+            res.status(401);
+            throw new Error('No refresh token');
+        }
+
+        const { payload } = await jwtVerify(token, JWT_SECRET);
+
+        const user = await User.findById(payload.userId);
+
+        if (!user) {
+            res.status(401);
+            throw new Error('No user');
+        }
+
+        const newAccessToken = await generateToken({userId: user._id.toString()}, '1m');
+
+        res.json({
+            accessToken: newAccessToken,
+            use: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+        })
+
+    } catch (err) {
+        res.status(401);
+        next(err);
+    }
+})
 
 
 export default router;
