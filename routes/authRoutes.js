601bec6d42aaeb643871e1b0d7e0e48dd3738786
Nicole <npereira@confidencialimobiliario.com>
2026-06-12 15:51:16 +0100

    Generate access and refresh tokens

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../models/User.js'
+import{ generateToken } from '../utils/generateToken.js'
 
 
 const router = express.Router();
@@ -24,10 +25,23 @@ router.post('/register', async (req, res, next) => {
             throw new Error('User already exists');
         }
 
-        const user = new User({name, email, password});
-        await user.save();
+        const user = await User({name, email, password});
+
+        //Create Tokens
+        const payload = {userId: user._id.toString()};
+        const accessToken = await generateToken(payload, '1m');
+        const refreshToken = await generateToken(payload, '30d');
+
+        // Set refresh token in HTTP-Only cookie
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none',
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        });
 
         res.status(201).json({
+            accessToken,
             user: {
                 id: user._id,
                 name: user.name,
