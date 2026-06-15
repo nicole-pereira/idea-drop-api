333db91638dd86946e069791c5cf282733e5e23c
Nicole <npereira@confidencialimobiliario.com>
2026-06-15 09:53:32 +0100

    Protecting routes with middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+import { JWT_SECRET } from '../utils/getJwtSecret.js';
+import { jwtVerify } from "jose";
+import User from '../models/User.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const protect = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            res.status(401);
+            throw new Error('Not authorized, no token');
+        }
+
+        const token = authHeader.spit(' ')[1];
+        const { payload } = await jwtVerify(token, JWT_SECRET);
+
+        const user = await User.findById(payload.userId).select('_id name email');
+
+        if (!user) {
+            res.status(401);
+            throw new Error('User not found');
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        res.status(401);
+        next(new Error('Not authorized, token failed'));
+    }
+}
--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 import Idea from '../models/Idea.js';
 import mongoose from 'mongoose';
+import { protect } from '../middleware/authMiddleware.js';
 
 // @route         GET /api/ideas
 // @description   Get all ideas
@@ -46,7 +47,7 @@ router.get('/:id', async (req, res, next) => {
 // @route         POST /api/ideas
 // @description   Create new idea
 // @access        Public
-router.post('/', async (req, res, next) => {
+router.post('/', protect, async (req, res, next) => {
   try {
     const { title, summary, description, tags } = req.body || {};
 
@@ -79,7 +80,7 @@ router.post('/', async (req, res, next) => {
 // @route         DELETE /api/ideas/:id
 // @description   Delete idea
 // @access        Public
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', protect, async (req, res, next) => {
   try {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -101,7 +102,7 @@ router.delete('/:id', async (req, res, next) => {
 // @route         PUT /api/ideas/:id
 // @description   Update idea
 // @access        Public
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', protect, async (req, res, next) => {
   try {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
